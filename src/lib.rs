1ef457fbdf8907de4608e12eb1e62d1046d472c2
SecondarySkyler <skylerhunter6@gmail.com>
2025-01-27 08:59:57 +0100

    print

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@ use std::collections::HashMap;
 
 use common::slc_commands::{ClientCommand, ClientEvent, ServerCommand, ServerEvent};
 use crossbeam_channel::{Receiver, Sender};
+use eframe::egui;
 use wg_2024::{
     controller::{DroneCommand, DroneEvent},
     network::NodeId,
 };
-use eframe::egui;
 
 pub struct MyApp {
     name: String,
@@ -34,12 +34,6 @@ impl eframe::App for MyApp {
 #[derive(Debug)]
 pub struct SimulationController {
     id: NodeId,
-    // drones: HashMap<NodeId, Sender<DroneCommand>>,
-    // drones_rcv: Receiver<DroneEvent>,
-    // clients: HashMap<NodeId, Sender<ClientCommand>>,
-    // clients_rcv: Receiver<ClientEvent>,
-    // servers: HashMap<NodeId, Sender<ServerCommand>>,
-    // servers_rcv: Receiver<ServerEvent>,
     drones_channels: HashMap<NodeId, (Sender<DroneCommand>, Receiver<DroneEvent>)>,
     clients_channels: HashMap<NodeId, (Sender<ClientCommand>, Receiver<ClientEvent>)>,
     servers_channels: HashMap<NodeId, (Sender<ServerCommand>, Receiver<ServerEvent>)>,
@@ -54,12 +48,6 @@ impl SimulationController {
     ) -> Self {
         SimulationController {
             id,
-            // drones,
-            // drones_rcv,
-            // clients,
-            // clients_rcv,
-            // servers,
-            // servers_rcv,
             drones_channels,
             clients_channels,
             servers_channels,
@@ -67,11 +55,13 @@ impl SimulationController {
     }
 
     pub fn run(&mut self) {
+        println!("Running simulation controller with drones: {}", self.drones_channels.len());
         let options = eframe::NativeOptions::default();
-        eframe::run_native("app",
-        options, Box::new(|cc| {
-            Box::new(MyApp::default())
-        }));
-        // todo!()
+        eframe::run_native(
+            "Simulation Controller",
+            options,
+            Box::new(|cc| Box::new(MyApp::default())),
+        )
+        .expect("Failed to run simulation controller");
     }
 }
