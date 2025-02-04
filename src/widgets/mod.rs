5028d7795e19dc29529aec973a692ef8a138a13c
SecondarySkyler <skylerhunter6@gmail.com>
2025-02-04 09:25:03 +0100

    added helper to send command for adding a neighbor

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,7 +1,9 @@
+use crossbeam_channel::Sender;
 use server_widget::ServerWidget;
 use drone_widget::DroneWidget;
 use web_client_widget::WebClientWidget;
 use chat_client_widget::ChatClientWidget;
+use wg_2024::packet::Packet;
 
 pub mod drone_widget;
 pub mod web_client_widget;
@@ -14,4 +16,15 @@ pub enum WidgetType {
     WebClient(WebClientWidget),
     ChatClient(ChatClientWidget),
     Server(ServerWidget),
+}
+
+impl WidgetType {
+    pub fn add_neighbor_helper(&mut self, nid: u8, nch: Sender<Packet>) {
+        match self {
+            WidgetType::Drone(drone_widget) => drone_widget.add_neighbor(nid, nch),
+            WidgetType::WebClient(web_client_widget) => web_client_widget.add_neighbor(nid, nch),
+            WidgetType::ChatClient(chat_client_widget) => chat_client_widget.add_neighbor(nid, nch),
+            WidgetType::Server(server_widget) => server_widget.add_neighbor(nid, nch),
+        }
+    }
 }
