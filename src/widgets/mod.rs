e6a6ecfcf19d1faa1c90ac5618c7101bc1d913d0
SecondarySkyler <skylerhunter6@gmail.com>
2025-02-04 10:43:09 +0100

    helper to get id of widget; helper to remove neighbor

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -3,7 +3,7 @@ use server_widget::ServerWidget;
 use drone_widget::DroneWidget;
 use web_client_widget::WebClientWidget;
 use chat_client_widget::ChatClientWidget;
-use wg_2024::packet::Packet;
+use wg_2024::{network::NodeId, packet::Packet};
 
 pub mod drone_widget;
 pub mod web_client_widget;
@@ -19,12 +19,30 @@ pub enum WidgetType {
 }
 
 impl WidgetType {
+    pub fn get_id_helper(&self) -> NodeId {
+        match self {
+            WidgetType::Drone(drone_widget) => drone_widget.get_id(),
+            WidgetType::WebClient(web_client_widget) => web_client_widget.get_id(),
+            WidgetType::ChatClient(chat_client_widget) => chat_client_widget.get_id(),
+            WidgetType::Server(server_widget) => server_widget.get_id(),
+        }
+    }
+
     pub fn add_neighbor_helper(&mut self, nid: u8, nch: Sender<Packet>) {
         match self {
             WidgetType::Drone(drone_widget) => drone_widget.add_neighbor(nid, nch),
             WidgetType::WebClient(web_client_widget) => web_client_widget.add_neighbor(nid, nch),
             WidgetType::ChatClient(chat_client_widget) => chat_client_widget.add_neighbor(nid, nch),
             WidgetType::Server(server_widget) => server_widget.add_neighbor(nid, nch),
+        }
+    }
+
+    pub fn rm_neighbor_helper(&mut self, neighbor_id: u8) {
+        match self {
+            WidgetType::Drone(drone_widget) => drone_widget.remove_neighbor(neighbor_id),
+            WidgetType::WebClient(web_client_widget) => web_client_widget.remove_neighbor(neighbor_id),
+            WidgetType::ChatClient(chat_client_widget) => chat_client_widget.remove_neighbor(neighbor_id),
+            WidgetType::Server(server_widget) => server_widget.remove_neighbor(neighbor_id),
         }
     }
 }
