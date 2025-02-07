413584df1662bd34ddd51e88134e266095c557c7
SecondarySkyler <skylerhunter6@gmail.com>
2025-02-07 13:29:24 +0100

    documentation

--- src/widgets/drone_widget.rs
+++ src/widgets/drone_widget.rs
@@ -1,16 +1,27 @@
 use crossbeam_channel::Sender;
-use egui::{Button, Color32, RichText, Ui, Widget};
+use egui::{Color32, RichText, Ui, Widget};
 use wg_2024::{controller::DroneCommand, network::NodeId, packet::Packet};
 
 #[derive(Clone, Debug)]
+/// Represents a drone widget
+/// 
+/// This struct stores the NodeId and the `Sender<DroneCommand>` of the
+/// represented drone.
+/// Furthermore, it stores the input for the packet drop rate (PDR) and a flag
+/// to indicate if the input is invalid.
 pub struct DroneWidget {
+    /// The NodeId of the drone
     id: NodeId,
+    /// The `Sender<DroneCommand>` channel to send commands to the drone
     command_ch: Sender<DroneCommand>,
+    /// The input field for the packet drop rate (PDR)
     pdr_input: String,
+    /// Flag to indicate if the input for the PDR is invalid
     is_pdr_invalid: bool,
 }
 
 impl DroneWidget {
+    /// Creates a new `DroneWidget` with the given `id` and `command_ch`
     pub fn new(
         id: NodeId,
         command_ch: Sender<DroneCommand>,
@@ -23,25 +34,45 @@ impl DroneWidget {
         }
     }
 
+    /// Utility function to send a `DroneCommand::AddSender` command to the drone
+    /// Adds a new neighbor with `neighbor_id` to the drone's neighbor list
+    /// Furthermore, a clone of the `Sender<Packet>` channel is stored in the drone
     pub fn add_neighbor(&mut self, neighbor_id: u8, neighbor_ch: Sender<Packet>) {
         self.command_ch
             .send(DroneCommand::AddSender(neighbor_id, neighbor_ch)).expect("msg not sent");
     }
 
+    /// Utility function to send a `DroneCommand::RemoveSender` command to the drone
+    /// Removes a the neighbor with `neighbor_id` from the drone's neighbor list
     pub fn remove_neighbor(&self, neighbor_id: u8) {
         self.command_ch
             .send(DroneCommand::RemoveSender(neighbor_id)).expect("msg not sent");
     }
 
+    /// Utility function to get the NodeId of the drone
     pub fn get_id(&self) -> NodeId {
         self.id
     }
 
+    /// Utility function to send a `DroneCommand::Crash` command to the drone
     pub fn send_crash_command(&self) {
         self.command_ch
             .send(DroneCommand::Crash).expect("msg not sent");
     }
 
+    /// Function that validates the input for the PDR
+    /// 
+    /// The input is considered valid if it is not empty and can be parsed as a float
+    /// between 0.0 and 1.0.
+    /// 
+    /// # Example
+    /// ```no_run
+    /// let pdr = "0.5".to_string();
+    /// assert_eq!(validate_parse_pdr(&pdr), Some(0.5));
+    /// 
+    /// let pdr = "1.5".to_string();
+    /// assert_eq!(validate_parse_pdr(&pdr), None);
+    /// ```
     fn validate_parse_pdr(&self, input_pdr: &String) -> Option<f32> {
         if input_pdr.is_empty() {
             return None;
@@ -56,7 +87,15 @@ impl DroneWidget {
     }
 }
 
-
+/// Implement the `egui::Widget` trait for `DroneWidget`
+/// 
+/// This allows the `DroneWidget` to be rendered as an egui widget
+/// 
+/// # Example
+/// ```no_run
+/// use egui::Ui;
+/// ui.add(DroneWidget::new(1, command_ch));
+/// ```
 impl Widget for DroneWidget {
     fn ui(mut self, ui: &mut Ui) -> egui::Response {
         ui.vertical(|ui| {
--- src/widgets/server_widget.rs
+++ src/widgets/server_widget.rs
@@ -4,12 +4,19 @@ use egui::{Ui, Widget};
 use wg_2024::{network::NodeId, packet::Packet};
 
 #[derive(Clone, Debug)]
+/// Represents a server widget
+/// 
+/// This struct stores the NodeId and the `Sender<ServerCommand>` of the 
+/// represented server. 
 pub struct ServerWidget {
+    /// The NodeId of the server
     pub id: NodeId,
+    /// The `Sender<ServerCommand>` channel to send commands to the server
     pub command_ch: Sender<ServerCommand>,
 }
 
 impl ServerWidget {
+    /// Creates a new `ServerWidget` with the given `id` and `command_ch`
     pub fn new(
         id: NodeId,
         command_ch: Sender<ServerCommand>,
@@ -20,21 +27,36 @@ impl ServerWidget {
         }
     }
 
+    /// Utility function to send a `ServerCommand::AddSender` command to the server
+    /// Adds a new neighbor with `neighbor_id` to the server's neighbor list
+    /// Furthermore, a clone of the `Sender<Packet>` channel is stored in the server
     pub fn add_neighbor(&mut self, neighbor_id: u8, neighbor_ch: Sender<Packet>) {
         self.command_ch
             .send(ServerCommand::AddSender(neighbor_id, neighbor_ch)).expect("msg not sent");
     }
 
+    /// Utility function to send a `ServerCommand::RemoveSender` command to the server
+    /// Removes a the neighbor with `neighbor_id` from the server's neighbor list
     pub fn remove_neighbor(&self, neighbor_id: u8) {
         self.command_ch
             .send(ServerCommand::RemoveSender(neighbor_id)).expect("msg not sent");
     }
 
+    /// Utility function to get the NodeId of the server
     pub fn get_id(&self) -> NodeId {
         self.id
     }
 }
 
+/// Implement the `egui::Widget` trait for `ServerWidget`
+/// 
+/// This allows the `ServerWidget` to be rendered as an egui widget
+/// 
+/// # Example
+/// ```no_run
+/// use egui::Ui;
+/// ui.add(ServerWidget::new(1, command_ch));
+/// ```
 impl Widget for ServerWidget {
     fn ui(mut self, ui: &mut Ui) -> egui::Response {
         ui.vertical_centered(|ui| {
