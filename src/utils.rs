202514616e21477a5b6f3341319d5dcfa781c1ea
SecondarySkyler <skylerhunter6@gmail.com>
2025-02-09 10:20:49 +0100

    docs + clippy

--- src/utils.rs
+++ src/utils.rs
@@ -1,19 +1,22 @@
+#![allow(clippy::len_without_is_empty)]
 use std::collections::VecDeque;
 
-
+/// A simple event queue that stores the last `capacity` events.
 pub struct EventQueue<T> {
     queue: VecDeque<T>,
     capacity: usize,
 }
 
 impl<T> EventQueue<T> {
-    pub fn new(capacity: usize) -> Self {
+    /// Create a new event queue with the given capacity.
+    #[must_use] pub fn new(capacity: usize) -> Self {
         EventQueue {
             queue: VecDeque::with_capacity(capacity),
             capacity,
         }
     }
 
+    /// Push a new event to the queue. If the queue is full, the oldest event will be removed.
     pub fn push(&mut self, event: T) {
         if self.queue.len() == self.capacity {
             self.queue.pop_front();
@@ -21,11 +24,13 @@ impl<T> EventQueue<T> {
         self.queue.push_back(event);
     }
 
-    pub fn get(&self) -> Vec<&T> {
+    /// Get all events in the queue.
+    #[must_use] pub fn get(&self) -> Vec<&T> {
         self.queue.iter().collect()
     }
 
-    pub fn len(&self) -> usize {
+    /// Get the number of events in the queue.
+    #[must_use] pub fn len(&self) -> usize {
         self.queue.len()
     }
 }
